b7e24ea31676d92d32d64b4368a5bb82db4cc402
Undi <maikajaneunde@gmail.com>
2025-11-20 08:57:59 +0800

    Fix postgresql connection

--- src/generated/client.ts
+++ src/generated/client.ts
@@ -38,9 +38,6 @@ export type PrismaClient<LogOpts extends Prisma.LogLevel = never, OmitOpts exten
 export { Prisma }
 
 
-// file annotations for bundling tools to include these files
-path.join(__dirname, "query_engine-windows.dll.node")
-path.join(process.cwd(), "src/generated/query_engine-windows.dll.node")
 
 /**
  * Model roles
