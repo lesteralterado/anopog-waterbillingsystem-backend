e622a3dad6159d5223f370e24518cf421e698a85
Undi <maikajaneunde@gmail.com>
2025-11-26 14:46:40 +0800

    Prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,12 @@
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
 
 export const prisma = new PrismaClient({
-  accelerateUrl: process.env.ACCELERATE_URL,
+  adapter,
   log: ["query", "warn", "error"],
 });
 
