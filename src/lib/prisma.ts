6616f3881036c0d927992acefe88b361e4bec980
Undi <maikajaneunde@gmail.com>
2025-11-26 09:46:22 +0800

    Fix error

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,7 @@
 import { PrismaClient } from '@prisma/client';
-import { Pool } from 'pg';
-import { PrismaPg } from '@prisma/adapter-pg';
-
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-const adapter = new PrismaPg(pool);
 
 export const prisma = new PrismaClient({
-  adapter,
+  accelerateUrl: process.env.ACCELERATE_URL,
   log: ["query", "warn", "error"],
 });
 
